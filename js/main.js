082ec56ac86ef7f592449b43e3fd530b2ebf4984
Eliascolin <eliascolina.c@gmail.com>
2023-01-26 11:11:01 -0300

    feita a estrutura de swiper-slide

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+var swiper = new Swiper(".slide-characters", {
+    slidesPerView: 3.5,
+    spaceBetween: 19,
+   
   });
