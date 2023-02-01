2ae10239dfeb9b4b422046bba60b6a7de3fafeaa
Eliascolin <eliascolina.c@gmail.com>
2023-02-01 12:33:51 -0300

    finalizado o responsivo

--- js/main.js
+++ js/main.js
@@ -2,5 +2,30 @@ var swiper = new Swiper(".slide-characters", {
     slidesPerView: 3.5,
     spaceBetween: 19,
     freeMode: true,
+    breakpoints:{
+     320:{
+      slidesPerView: 1.3,
+      
+     },
+
+     768:{
+      slidesPerView: 2.3
+     }, 
+
+
+     991:{
+      slidesPerView: 2.8,
+      
+     },
+     1200:{
+      slidesPerView: 3.5,
+      
+     },
+
+
+     
+
+
+    }
    
   });
